036cf450123e5a08a98db296c2e0934672dc137e
Carlos Daniel Hernandez <chernandez@agglobal.com>
2021-09-11 16:55:04 -0600

    Apply new components for web app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListaEmpleadoComponent } from './components/lista-empleado/lista-empleado.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'listar-empleados',
+    component: ListaEmpleadoComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListaEmpleadoComponent } from './components/lista-empleado/lista-empleado.component';
+import { CrearEmpleadoComponent } from './components/crear-empleado/crear-empleado.component';
+import { ActualizarEmpleadoComponent } from './components/actualizar-empleado/actualizar-empleado.component';
+import { NavegacionComponent } from './shared/navegacion/navegacion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaEmpleadoComponent,
+    CrearEmpleadoComponent,
+    ActualizarEmpleadoComponent,
+    NavegacionComponent
   ],
   imports: [
     BrowserModule,
